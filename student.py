e75201d94a1a8e559075f02864a5a5520606bb81
aradhanabhoyar <168920602+aradhanabhoyar@users.noreply.github.com>
2025-06-25 11:04:54 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,12 @@
+from student_module import Student
+
+subjects = {
+    "English": 56,
+    "Math": 76,
+    "Science": 98,
+    "History": 65,
+    "Geography": 96
+}
+
+student1 = Student("Ram", 1 , subjects)
+student1.display_info()
